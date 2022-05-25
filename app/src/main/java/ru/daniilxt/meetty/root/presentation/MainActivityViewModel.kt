4fbfdce415bba345200efc9d5e3309c7401827ea
Daniil Firsov <daniil-firsov@mail.ru>
2022-05-26 02:38:03 +0300

    Bottom nav buttons listener

--- app/src/main/java/ru/daniilxt/meetty/root/presentation/MainActivityViewModel.kt
+++ app/src/main/java/ru/daniilxt/meetty/root/presentation/MainActivityViewModel.kt
@@ -2,4 +2,13 @@ package ru.daniilxt.meetty.root.presentation
 
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel : ViewModel()
+class MainActivityViewModel : ViewModel() {
+    fun openMessagesFragment() {
+    }
+
+    fun openHome() {
+    }
+
+    fun search() {
+    }
+}
